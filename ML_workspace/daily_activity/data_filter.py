3a2f0db1000dea7e7dc9a56ca7c323fedf641bc8
Muhammad Arjuna Wirakusuma <muhammad.arjuna.wirakusuma@mail.ugm.ac.id>
2024-12-10 13:51:25 +0700

    edit model

--- ML_workspace/daily_activity/data_filter.py
+++ ML_workspace/daily_activity/data_filter.py
@@ -3,23 +3,20 @@ import numpy as np
 import tensorflow as tf 
 
 
-def get_frames(df, frame_size, hop_size):
+def get_frames(df):
+    frame_size = Fs*4 
+    hop_size = Fs*2
     encoder = OneHotEncoder(sparse_output=False)
     N_FEATURES = 3
     frames = []
-    labels = []
     for i in range(0,len(df )- frame_size, hop_size):
         x = df['x'].values[i: i+frame_size]
         y = df['y'].values[i: i+frame_size]
         z = df['z'].values[i: i+frame_size]
-        
-        label = stats.mode(df['label'][i: i+frame_size])[0]
         frames.append([x,y,z])
-        labels.append(label)
         
     frames = np.asarray(frames).reshape(-1, frame_size, N_FEATURES)
     frames = np.expand_dims(frames,axis=1)
-    labels = np.asarray(labels).reshape(-1,1)
-    labels = encoder.fit_transform(labels)
+
     
-    return frames, labels
+    return frames
